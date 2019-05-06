c5403216b81b561bd8962c894e24c9e8033d45dc
spetitcuenot <spetitcuenot@magic.fr>
2019-05-06 18:40:25 +0200

    pm maj

---
+++ 5-0-print_memory-ok/print_memory.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   print_memory.c                                   .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: spetitcu <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/05/06 15:31:38 by spetitcu     #+#   ##    ##    #+#       */
+/*   Updated: 2019/05/06 15:32:17 by spetitcu    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_memory(const void *addr, size_t size)
+{
+	size_t			i;
+	size_t 			j;
+	unsigned char	*p;
+	char 			*str;
+
+	str = "0123456789abcdef"; 
+	p = (unsigned char *)addr;
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < 16 && i + j < size)
+		{
+			write(1, &str[(*(p + i + j)/16) % 16], 1);
+			write(1, &str[*(p + i + j) % 16], 1);
+			if (j % 2)
+				write(1, " ", 1);
+			j++;
+		}
+		while (j < 16)
+		{
+			write(1, "  ", 2);
+			if (j % 2)
+				write(1, " ", 1);
+			j++;
+		}
+		j = 0;
+		while (j < 16 && i + j < size)
+		{
+			if (*(p + i + j) >= 32 && *(p + i + j) < 127)
+				write(1, p + i + j, 1);
+			else
+				write(1, ".", 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i += 16;
+	}
+}
